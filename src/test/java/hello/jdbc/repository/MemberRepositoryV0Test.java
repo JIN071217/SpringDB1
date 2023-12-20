9f8c466971d51792f2bff7cb1b4741f4d10c10a5
new git project <ld4130@naver.com>
2023-12-20 23:56:11 +0900

    Java Database Connection 이해 1. 데이터베이스 연결 2. JDBC 개발 - 등록(H2) 3. JDBC 개발 - 조회 4. JDBC 개발 - 수정,삭제

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -4,10 +4,13 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -18,7 +21,7 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV5", 10000);
+        Member member = new Member("memberV300", 10000);
         repository.save(member);
 
         //findById
@@ -30,5 +33,10 @@ class MemberRepositoryV0Test {
         repository.update(member.getMemberId(), 20000);
         Member updateMember = repository.findById((member.getMemberId()));
         assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
